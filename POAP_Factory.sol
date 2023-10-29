0622b96fa5bf2b96b1568ffdd6733537705371fd
0xSorcerers <121747420+0xsorcerers@users.noreply.github.com>
2023-10-29 16:32:15 +0100

    Create POAP_Factory.sol

---
+++ POAP_Factory.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: No license
+
+// @title Non-transferable POAP NFT 
+// https://twitter.com/0xSorcerers | https://github.com/Dark-Viper | https://t.me/Oxsorcerer | https://t.me/battousainakamoto | https://t.me/darcViper
+
+pragma solidity ^0.8.17;
