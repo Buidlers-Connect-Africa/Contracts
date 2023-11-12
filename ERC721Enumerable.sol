037fbd640f13ee4b70b8c26feb18d844a1f0d053
0xSorcerers <121747420+0xsorcerers@users.noreply.github.com>
2023-11-12 15:11:15 +0100

    Create ERC721Enumerable.sol

---
+++ ERC721Enumerable.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.8.0) (token/ERC721/extensions/ERC721Enumerable.sol)
+
+pragma solidity ^0.8.0;
+
+import "./ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
